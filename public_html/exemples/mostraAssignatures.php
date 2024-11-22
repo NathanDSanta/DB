8b611d77bee41796e6f8f0a14a653911e33e0a30
NathanDSanta <natan.d.santa@gmail.com>
2024-11-22 16:01:04 +0100

    public_html added

---
+++ public_html/exemples/mostraAssignatures.php
@@ -0,0 +1,48 @@
+#!/usr/bin/php-cgi
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+   <title>Exemple molt simple PHP i Oracle</title>
+   <link rel="stylesheet" href="exemple.css" type="text/css"> 
+</head>
+<body>
+<h1>Contingut de la taula d'assignatures</h1>
+<?php
+    $usuari=$_POST['usuari'];
+    $password=$_POST['password'];
+    $BD='ORCLCDB';
+    echo "<p>Dades agafades de la base de dades [<b>" . $BD . 
+         "</b>] des de l'usuari [<b>" . $usuari . "</b>]</p>\n";
+    $conn = oci_connect($usuari, $password, $BD);
+	$consulta="SELECT codi, nom, credits FROM Assignatures";
+	if ($_POST['ordenacio']=='sense'){
+	   echo "<p>Files mostrades sense forçar cap ordenació</p>\n";
+	} else {
+	   echo "<p>Files mostrades ordenades per <b>".$_POST['ordenacio']."</b></p>\n";
+	   $consulta=$consulta . " ORDER BY ".$_POST['ordenacio'];
+	}
+	$comanda = oci_parse($conn, $consulta);
+	$exit=oci_execute($comanda);
+	$numColumnes=oci_num_fields($comanda);
+	// mostrem les capceleres
+	echo "<table>\n";
+	echo "  <tr>";
+	for ($i=1;$i<=$numColumnes; $i++) {
+		echo "<th>".oci_field_name($comanda,$i) . "</th>"; 
+	}
+	echo "</tr>\n";
+	// recorrem les files
+	while (($row = oci_fetch_array($comanda, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+		echo "  <tr>";
+		foreach ($row as $element) {
+			echo "<td>".$element . "</td>";
+		}
+		echo "</tr>\n";
+	}
+	echo "</table>\n";
+	oci_free_statement($comanda);
+	oci_close($conn);
+?>
+</body>
+</html>
---
+++ public_html/miniweb/menu.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php-cgi
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+  <title>Exemple PHP: menú</title>
+  <link rel="stylesheet" href="exemple.css" type="text/css"> 
+</head>
+<body>
+<?php
+  include 'funcions.php';
+  iniciaSessio();
+  // emmagatzem usuari i password en una sessió per tenir-los disponibles 
+  if (!empty($_POST['username'])) { // Hem arribat des de index.html
+    $_SESSION['usuari'] = $_POST['username'];
+    $_SESSION['password'] = $_POST['password'];
+    // ara comprovem usuari i password intentant establir connexió amb Oracle    
+    connecta($conn);
+   }
+?>
+  <h1>Exemple PHP+SQL Oracle+HTML curs 24/25</h1>
+  <h2>Operacions disponibles</h2>
+  <p> <a class="menu" href="mostrarAssignatures.php">a) Mostrar totes les assignatures</a></p>
+  <p> <a class="menu" href="afegirAssignatura.php">b) Afegir una nova assignatura</a></p>
+  <p> <a class="menu" href="qualificarAssignatura.php">c) Qualificar assignatura</a></p>
+  <p> <a class="menu" href="mostrarExpedient.php">d) Mostrar expedient alumnes per edat</a></p>  
+  <?php peu("Tornar a la pàgina de login","index.html");?>
+</body>
+</html>
