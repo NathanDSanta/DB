bbc8c3eea0a905826824ab5f230021cc93edac18
NathanDSanta <natan.d.santa@gmail.com>
2024-12-27 14:11:06 +0100

    added order by

--- public_html/practica/mostrarVehicles.php
+++ public_html/practica/mostrarVehicles.php
@@ -15,7 +15,9 @@
     $consulta="select v.codi, v.descripcio, v.color, v.combustible, v.consum, v.consum*c.preuunitat as costcombustible100km, u.nom, u.cognoms 
     from vehicles v 
     join combustibles c on v.combustible=c.descripcio 
-    left outer join usuaris u on v.propietari=u.alias";
+    left outer join usuaris u on v.propietari=u.alias
+    order by v.codi; 
+    ";
 //	$consulta="SELECT * FROM Assignatures WHERE codi='aaa"; //error oci_parse
 //  $consulta="SELECT * FROM Assignatur"; // error oci_execute             
 	$comanda = oci_parse($conn, $consulta);
