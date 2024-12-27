d9b29935616ea7df7616ee3a972bfd15f15f9262
NathanDSanta <natan.d.santa@gmail.com>
2024-12-27 13:50:04 +0100

    afegirVehicle_BD.php

--- public_html/practica/afegirVehicle_BD.php
+++ public_html/practica/afegirVehicle_BD.php
@@ -3,7 +3,7 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
-  <title>Exemple PHP: afegir Assignatura, inserció a la base de dades</title>
+  <title>Practica PHP: Donar d'alta un Vehicle, inserció a la base de dades</title>
   <link rel="stylesheet" href="exemple.css" type="text/css"> 
 </head>
 <body>
@@ -11,34 +11,43 @@
   include 'funcions.php';     
   iniciaSessio();
   connecta($conn);
-  capcalera("Inserir assignatura a la base de dades"); 
-  $consultaCodi="SELECT codi, nom FROM Assignatures WHERE codi=:codiAssignatura";
+  capcalera("Inserir Vehicle a la base de dades"); 
+  $consultaCodi="SELECT codi, descripcio FROM Vehicles WHERE codi=:codiVehicle";
   $comanda = oci_parse($conn, $consultaCodi);
-  oci_bind_by_name($comanda,":codiAssignatura",$_POST["codi"]);
+  $codi = substr($_POST["grupvehicle"], 0, 3) . substr($_POST["descripcio"], 0, 6);
+  $codi = str_replace(' ', '_', $codi);
+  oci_bind_by_name($comanda,":codiVehicle",$_POST["codi"]);
   $exit = oci_execute($comanda);
   $fila=oci_fetch_array($comanda); // no fem control d'errors 
+  while ($fila) {
+    $codi = $codi . rand(0, 999);
+    oci_bind_by_name($comanda,":codiVehicle",$_POST["codi"]);
+    $exit = oci_execute($comanda);
+    $fila=oci_fetch_array($comanda); // no fem control d'errors 
+  }
   if (!$fila){ // no existeix cap assignatura amb el codi rebut 
     oci_free_statement($comanda);
-    $sentenciaSQL = "INSERT INTO Assignatures (codi, nom, credits, responsable, semestre, curs, tipus, carrera) 
-                     VALUES (:codi, :nom, :credits, :responsable, :semestre, :curs, :tipus, :carrera)";
+    $sentenciaSQL = "INSERT INTO Vehicles (codi, descripcio, color, consum, datacompra, preu, grupvehicle, combustible, propietari) 
+                     VALUES (:codi, :descripcio, :color, :consum, :datacompra, :preu, :grupvehicle, :combustible, :propietari)";
     $comanda = oci_parse($conn, $sentenciaSQL);
-    oci_bind_by_name($comanda, ":codi", $_POST["codi"]);
-    oci_bind_by_name($comanda, ":nom", $_POST["nom"]);
-    oci_bind_by_name($comanda, ":credits", $_POST["credits"]);
-    oci_bind_by_name($comanda, ":responsable", $_POST["responsable"]);
-    oci_bind_by_name($comanda, ":semestre", $_POST["semestre"]);
-    oci_bind_by_name($comanda, ":curs", $_POST["curs"]);
-    oci_bind_by_name($comanda, ":tipus", $_POST["tipus"]);
-    oci_bind_by_name($comanda, ":carrera", $_POST["carrera"]);
+    oci_bind_by_name($comanda, ":codi", $codi);
+    oci_bind_by_name($comanda, ":descripcio", $_POST["descripcio"]);
+    oci_bind_by_name($comanda, ":color", $_POST["color"]);
+    oci_bind_by_name($comanda, ":consum", $_POST["consum"]);
+    oci_bind_by_name($comanda, ":datacompra", $_POST["datacompra"]);
+    oci_bind_by_name($comanda, ":preu", $_POST["preu"]);
+    oci_bind_by_name($comanda, ":grupvehicle", $_POST["grupvehicle"]);
+    oci_bind_by_name($comanda, ":combustible", $_POST["combustible"]);
+    oci_bind_by_name($comanda, ":propietari", $_POST["propietari"]);
     $exit = oci_execute($comanda); 
     if ($exit) {
-        echo "<p>Nova assignatura amb codi " . $_POST['codi'] . " inserida a la base de dades</p>\n";
+        echo "<p>Nou vehicle amb codi " . $_POST['codi'] . " inserit a la base de dades</p>\n";
     } else {
         mostraErrorExecucio($comanda);
     }
   } else {
-      echo "<strong>COMPTE! Assigatura no creada:</strong> ja existia una assignatura amb codi ".
-           $_POST['codi']." amb nom [".$fila['NOM']."]\n";
+      echo "<strong>COMPTE! Vehicle ".
+           $_POST['codi']. " no creat</strong>\n";
   }
   oci_free_statement($comanda);
   oci_close($conn);
