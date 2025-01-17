13ce654ee9f52b4604f2e02e40d2750ddcf70bf6
NathanDSanta <natan.d.santa@gmail.com>
2025-01-17 11:21:42 +0100

    missed a constraint keyword

--- public_html/practica/practicaPHP.sql
+++ public_html/practica/practicaPHP.sql
@@ -4,12 +4,12 @@ SET feedback off
 
 DROP TABLE revisions;
 
-CREATE TABLE revisions (
+CREATE TABLE Revisions (
   codiVehicle varchar2(10) not null,
   data date default null,
   curses number(4,0) default null,
   personatge varchar2(15) default null,
-  PRIMARY KEY(codiVehicle,data),
+  constraint cp_revisions PRIMARY KEY(codiVehicle,data),
   constraint cf_revisions_codiVehicle
     FOREIGN KEY(codiVehicle) 
     REFERENCES vehicles(codi), 
