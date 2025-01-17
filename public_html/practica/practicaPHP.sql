642564f6e7bbda135563c48dd9eb997a0faf57fd
NathanDSanta <natan.d.santa@gmail.com>
2025-01-17 11:17:50 +0100

    script done, i think

---
+++ public_html/practica/practicaPHP.sql
@@ -0,0 +1,19 @@
+SET ECHO OFF
+SET termout on
+SET feedback off
+
+DROP TABLE revisions;
+
+CREATE TABLE revisions (
+  codiVehicle varchar2(10) not null constraint cp_revisions primary key,
+  data date default null,
+  curses number(4,0) default null,
+  personatge varchar2(15) default null,
+  PRIMARY KEY(codiVehicle,data),
+  constraint cf_revisions_codiVehicle
+    FOREIGN KEY(codiVehicle) 
+    REFERENCES vehicles(codi) 
+  constraint cf_revisions_personatge
+    FOREIGN KEY(personatge) 
+    REFERENCES personatges(alias) 
+);
